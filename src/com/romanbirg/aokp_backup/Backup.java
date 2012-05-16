45f8d543c43c6caa765c67fcf81cf25a2443dd6f
Roman Birg <romanbirg@gmail.com>
2012-05-16 18:42:46 -0500

    moar work

--- src/com/romanbirg/aokp_backup/Backup.java
+++ src/com/romanbirg/aokp_backup/Backup.java
@@ -1,3 +1,4 @@
+
 package com.romanbirg.aokp_backup;
 
 import java.io.BufferedWriter;
@@ -15,99 +16,109 @@ import android.util.Log;
 
 public class Backup {
 
-	public static final String TAG = "Backup";
+    public static final String TAG = "Backup";
 
-	// categories
-	public static final int CAT_GENERAL_UI = 0;
-	public static final int CAT_NAVIGATION_BAR = 1;
-	public static final int CAT_LOCKSCREEN_OPTS = 2;
-	public static final int CAT_POWER_MENU_OPTS = 3;
-	public static final int CAT_WEATHER = 4;
+    // categories
+    public static final int CAT_GENERAL_UI = 0;
+    public static final int CAT_NAVIGATION_BAR = 1;
+    public static final int CAT_LOCKSCREEN_OPTS = 2;
+    public static final int CAT_POWER_MENU_OPTS = 3;
+    public static final int CAT_WEATHER = 4;
 
-	Context mContext;
-	ArrayList<ArrayList<SVal>> backupValues = new ArrayList<ArrayList<SVal>>();
+    Context mContext;
+    ArrayList<ArrayList<SVal>> backupValues = new ArrayList<ArrayList<SVal>>();
+    boolean[] catsToBackup = new boolean[5];
 
-	public Backup(Context c) {
-		mContext = c;
-	}
+    public Backup(Context c, boolean[] categories) {
+        mContext = c;
+        catsToBackup = categories;
+    }
 
-	public void backupSettings(int category) {
-		Resources res = mContext.getResources();
-		ContentResolver resolver = mContext.getContentResolver();
-		ArrayList<SVal> vals = new ArrayList<SVal>();
-		String[] settings = null;
-		switch (category) {
-		case CAT_GENERAL_UI:
-			settings = res.getStringArray(R.array.cat_general_ui);
-		}
+    public void backupSettings() {
+        for (int i = 0; i < catsToBackup.length; i++) {
+            if (catsToBackup[i]) {
+                backupSettings(i);
+            }
+        }
+    }
 
-		if (settings == null) {
-			Log.e(TAG, "couldn't find array of settings for category: "
-					+ category);
-			return;
-		}
+    private void backupSettings(int category) {
+        Resources res = mContext.getResources();
+        ContentResolver resolver = mContext.getContentResolver();
+        ArrayList<SVal> vals = new ArrayList<SVal>();
+        String[] settings = null;
+        switch (category) {
+            case CAT_GENERAL_UI:
+                settings = res.getStringArray(R.array.cat_general_ui);
+        }
 
-		for (String setting : settings) {
-			if (!shouldHandleSpecialCase(setting)) {
-				String val = Settings.System.getString(resolver, setting);
-				if (val != null)
-					vals.add(new SVal(setting, val));
-			}
-		}
-		backupValues.add(category, vals);
-	}
+        if (settings == null) {
+            Log.e(TAG, "couldn't find array of settings for category: "
+                    + category);
+            return;
+        }
 
-	public boolean writeBackupSetings(String backupName) {
-		StringBuilder output = new StringBuilder();
+        for (String setting : settings) {
+            if (!shouldHandleSpecialCase(setting)) {
+                String val = Settings.System.getString(resolver, setting);
+                if (val != null)
+                    vals.add(new SVal(setting, val));
+            }
+        }
+        backupValues.add(category, vals);
+    }
 
-		for (ArrayList<SVal> array : backupValues) {
-			for (SVal pair : array) {
-				output.append(pair.setting + "=" + pair.val + "\n");
-			}
-			output.append("\n");
-		}
+    public boolean writeBackupSetings(String backupName) {
+        StringBuilder output = new StringBuilder();
 
-		File dir = new File(mContext.getExternalFilesDir(null), backupName);
-		File backup = new File(dir, "");
-		Writer outWriter;
-		try {
-			outWriter = new BufferedWriter(new FileWriter(backup));
-			outWriter.write(output.toString());
-			outWriter.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
+        for (ArrayList<SVal> array : backupValues) {
+            for (SVal pair : array) {
+                output.append(pair.setting + "=" + pair.val + "\n");
+            }
+            output.append("\n");
+        }
 
-	public boolean shouldHandleSpecialCase(String setting) {
-		if (setting.equals("disable_boot_animation")) {
+        File dir = new File(mContext.getExternalFilesDir(null), backupName);
+        File backup = new File(dir, "");
+        Writer outWriter;
+        try {
+            outWriter = new BufferedWriter(new FileWriter(backup));
+            outWriter.write(output.toString());
+            outWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 
-			return true;
-		} else if (setting.equals("disable_boot_audio")) {
+    public boolean shouldHandleSpecialCase(String setting) {
+        if (setting.equals("disable_boot_animation")) {
 
-			return true;
-		} else if (setting.equals("disable_bug_mailer")) {
+            return true;
+        } else if (setting.equals("disable_boot_audio")) {
 
-			return true;
-		}
+            return true;
+        } else if (setting.equals("disable_bug_mailer")) {
 
-		return false;
-	}
+            return true;
+        }
 
-	public static class SVal {
-		String setting;
-		String val;
+        return false;
+    }
 
-		public SVal(String setting, String val) {
-			this.setting = setting;
-			this.val = val;
-		}
+    public static class SVal {
+        String setting;
+        String val;
 
-		public String toString() {
-			return setting + "=" + val;
-		}
-	}
+        public SVal(String setting, String val) {
+            this.setting = setting;
+            this.val = val;
+        }
+
+        public String toString() {
+            return setting + "=" + val;
+        }
+    }
 
 }
--- src/com/romanbirg/aokp_backup/Home.java
+++ src/com/romanbirg/aokp_backup/Home.java
@@ -1,6 +1,8 @@
 
 package com.romanbirg.aokp_backup;
 
+import java.util.ArrayList;
+
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.Activity;
@@ -10,8 +12,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 public class Home extends Activity {
 
@@ -37,30 +42,55 @@ public class Home extends Activity {
     }
 
     public static class BackupFragment extends Fragment {
-        
+
+        String[] cats;
+        ArrayList<CheckBox> checkBoxes = new ArrayList<CheckBox>();
+
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setHasOptionsMenu(true);
+
         }
-        
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-        	
-        	View v = inflater.inflate(R.layout.main, null);
 
-        	
-        	
+            View v = inflater.inflate(R.layout.main, null);
+            LinearLayout categories = (LinearLayout) v.findViewById(R.id.categories);
+            cats = getActivity().getApplicationContext().getResources()
+                    .getStringArray(R.array.categories);
+            for (int i = 0; i < cats.length; i++) {
+                CheckBox b = new CheckBox(getActivity());
+                b.setTag(cats[i]);
+                categories.addView(b);
+            }
+
             return v;
         }
 
         @Override
         public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-
             inflater.inflate(R.menu.backup, menu);
             super.onCreateOptionsMenu(menu, inflater);
         }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.menu_backup:
+                    backup();
+
+            }
+            return super.onOptionsItemSelected(item);
+        }
+
+        public void backup() {
+            
+            Backup b = new Backup(Context, );
+        }
+
     }
 
     public static class RestoreFragment extends Fragment {
