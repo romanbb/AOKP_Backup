9fe1420be988b53431ed3add1dc46fb9547f5c7d
Roman Birg <romanbirg@gmail.com>
2012-05-24 13:12:48 -0500

    add Apache 2.0 license

--- src/com/aokp/backup/RestoreFragment.java
+++ src/com/aokp/backup/RestoreFragment.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2012 Roman Birg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.aokp.backup;
 
@@ -23,8 +38,6 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.Toast;
-
-
 
 public class RestoreFragment extends Fragment {
 
@@ -208,9 +221,14 @@ public class RestoreFragment extends Fragment {
                 }
             };
             files = backupDir.listFiles(fileFilter);
-            fileIds = new String[files.length];
-            for (int i = 0; i < files.length; i++) {
-                fileIds[i] = files[i].getName();
+            if (files == null) {
+                files = new File[0];
+                fileIds = new String[0];
+            } else {
+                fileIds = new String[files.length];
+                for (int i = 0; i < files.length; i++) {
+                    fileIds[i] = files[i].getName();
+                }
             }
         }
 
